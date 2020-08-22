80e8f9e489b2ecae4dc9e0da455b333d16be6b44
plesett <1540907804@qq.com>
2020-08-22 16:04:58 +0800

    更新&&

--- umi-dva/src/components/Sider/index.tsx
+++ umi-dva/src/components/Sider/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import style from './style.less'
+import { createFromIconfontCN } from '@ant-design/icons';
+
+const IconFont = createFromIconfontCN({
+    scriptUrl: '//at.alicdn.com/t/font_1769370_61bvdoostwd.js',
+});
 
 class SiderComponents extends React.Component {
     render() {
         return (
             <div className={style.sider}>
-                <p>推荐</p>
+                <p className={style.title}>推荐</p>
+                <div className={style.title_text_box}>
+                    <div id={style.action}><IconFont type="icon-yinyue" className={style.icon_info} />发现音乐</div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
             </div>
         )
     }
